13dacd7c00d4f01393a321817be78a62b7e9dde2
mdrahathossenantor <md.rahathossenantor@gmail.com>
2023-10-03 20:46:46 +0600

    Initial project setup completed with all dependencies

---
+++ README.md
@@ -0,0 +1,1 @@
+<h2>Welcome to Grand Reunion</h2>
---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './components/Home.jsx'
+import Register from './components/Register.jsx'
+import Login from './components/Login.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/", element: <App></App>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/register",
+        element: <Register></Register>
+      },
+      {
+        path: "/login",
+        element: <Login></Login>
+      }
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
